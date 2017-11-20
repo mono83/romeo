2b1d8515ff0091362223ff8dc5a7697a3e3b14a4
Gotterdemarung <gotterdemarung@gmail.com>
2017-11-20 13:38:04 +0200

    service container

---
+++ services/container.go
@@ -0,0 +1,92 @@
+package services
+
+import (
+	"sync"
+	"time"
+
+	"github.com/mono83/romeo"
+	"github.com/mono83/romeo/env"
+	"github.com/mono83/xray"
+	"github.com/mono83/xray/args"
+)
+
+// Container is special service, that is just slice of other services
+type Container []romeo.Service
+
+// GetName returns service's name, for this case it would be "container"
+func (Container) GetName() string { return "container" }
+
+// Register registers one or more services within container
+func (c Container) Register(services ...romeo.Service) error {
+	c = append(c, services...)
+	return nil
+}
+
+// Start starts services container
+func (c Container) Start(ray xray.Ray) error {
+	ray = ray.WithLogger("container")
+
+	ray.Debug("Starting services container")
+
+	allBefore := time.Now()
+	for _, group := range romeo.GroupByRunLevel(c, false) {
+		rl := romeo.RunLevelForService(group[0])
+		wg := sync.WaitGroup{}
+		wg.Add(len(group))
+		ray.Debug("Starting :count services on run level :level", args.Count(len(group)), args.String{N: "level", V: rl.String()})
+
+		var resultingError error
+		for _, service := range group {
+			go func(s romeo.Service) {
+				serviceLog := ray.With(args.Name(romeo.NameForService(s)))
+				serviceLog.Debug("Starting service :name")
+				before := time.Now()
+				if err := s.Start(serviceLog); err != nil {
+					serviceLog.Error("Service :name start failed with :err", args.Error{Err: err})
+					if resultingError == nil {
+						resultingError = err
+					}
+				} else {
+					serviceLog.Info("Service :name started in :delta", args.Delta(time.Now().Sub(before)))
+				}
+				wg.Done()
+			}(service)
+		}
+		wg.Wait()
+
+		if resultingError != nil {
+			return resultingError
+		}
+	}
+
+	ray.Info("Startup sequence done in :delta without errors", args.Delta(time.Now().Sub(allBefore)))
+	return nil
+}
+
+// Stop stops services container
+func (c Container) Stop(ray xray.Ray) error {
+	ray = ray.WithLogger("container")
+
+	ray.Debug("Stopping services container")
+
+	allBefore := time.Now()
+	for _, group := range romeo.GroupByRunLevel(c, true) {
+		rl := romeo.RunLevelForService(group[0])
+		wg := sync.WaitGroup{}
+
+		for _, service := range group {
+			go func(s romeo.Service) {
+				serviceLog := ray.With(args.Name(romeo.NameForService(s)))
+				serviceLog.Debug("Stopping service :name on :level", args.String{N: "level", V: rl.String()})
+				if err := s.Stop(serviceLog); err != nil {
+					serviceLog.Error("Service :name shutdown failed with :err", args.Error{Err: err})
+				}
+				wg.Done()
+			}(service)
+		}
+		wg.Wait()
+	}
+
+	ray.Info("Shutdown sequence done in :delta. PID :pid", args.Delta(time.Now().Sub(allBefore)), env.PID)
+	return nil
+}
